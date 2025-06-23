ccde3ce84394feadbd72737f4cb40db1688b5570
Raymond Abiola <raymondintell@gmail.com>
2025-06-23 21:04:28 +0100

    First Commit_ todolist project

---
+++ src/ToDoList.sol
@@ -0,0 +1,185 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import {AccessControl} from "../lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
+
+contract ToDoList is AccessControl{
+
+struct Task{
+    uint id;
+    string description;
+    bool isCompleted;
+}
+
+address public owner;
+address[] internal users;
+bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+uint private newTaskId;
+
+mapping(address => Task[]) internal userTasks;
+mapping (address => bool) internal isUser;
+mapping(address => bool) internal isBanned;
+
+event UserBanned(address indexed addr);
+event UserUnbanned(address indexed addr);
+
+error InvalidIndex();
+error InvalidAddress();
+error InvalidDescription();
+error  RestrictedUser();
+error TaskIsCompleted();
+error NoTaskFound();
+error IndexNotFound();
+error NotYetUser();
+
+constructor(){
+newTaskId = 1;
+owner = msg.sender;
+_grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+_grantRole(ADMIN_ROLE, msg.sender);
+}
+
+modifier validIndex(uint _index){
+    require(_index<userTasks[msg.sender].length, InvalidIndex());
+    _;
+}
+
+modifier validDescription(string memory _description){
+    require(bytes(_description).length>0, InvalidDescription());
+    _;
+}
+
+modifier notCompleted(uint _index){
+       require(!userTasks[msg.sender][_index].isCompleted, TaskIsCompleted());
+       _;
+}
+
+modifier validAddress(address _addr){
+    if(_addr == address(0)) revert InvalidAddress(); //If statements are preferred for reverts like this when testing with foundry
+    _;
+}
+
+function assignRole(bytes32 role, address addr)public validAddress(addr){
+    grantRole(role, addr);
+}
+                         
+function revokeRoleOfAdmin(bytes32 role, address addr) public validAddress(addr){
+revokeRole(role, addr);
+}
+
+// This function helps users add new tasks.
+function addTask(string memory _description)public validDescription(_description){
+    require(!isBanned[msg.sender], RestrictedUser());
+    Task memory newTask = Task({
+        id: newTaskId,
+        description: _description,
+        isCompleted: false
+    });
+    userTasks[msg.sender].push(newTask);
+    newTaskId++;
+
+    if(!isUser[msg.sender]){
+        users.push(msg.sender);
+        isUser[msg.sender] = true;
+    }
+}
+
+// This function helps users to edit their task
+function editTask(string memory _description, uint _index)public validIndex(_index) validDescription(_description) notCompleted(_index){
+userTasks[msg.sender][_index].description = _description;
+}
+
+// This function helps to mark a task complete
+function markTaskCompleted (uint _index) public validIndex(_index) notCompleted(_index){
+    Task[] storage tasks = userTasks [msg.sender];
+    tasks[_index].isCompleted = true;
+}
+
+// This function allows users to delete their task
+function delTask(uint _index)public validIndex(_index){
+    Task[] storage tasks = userTasks[msg.sender];
+    require (tasks.length!=0, NoTaskFound());
+for(uint i=_index; i<tasks.length-1; i++){
+tasks[i] = tasks[i+1];
+    }
+     tasks.pop();
+    }
+//Internal function to get the index of an address in users array
+function getAddressIndex(address targetValue)internal view returns(uint){
+for(uint i=0; i<users.length; i++){
+    if(users[i]==targetValue){
+        return i;
+    }
+}
+ revert IndexNotFound();
+}
+
+//Function to ban a user from this contract.
+function banUser(address _addr)public onlyRole(ADMIN_ROLE) {
+    require(isUser[_addr] == true, NotYetUser());
+uint addressIndex = getAddressIndex(_addr);
+for(uint i=addressIndex; i<users.length-1;i++){
+        users[i] = users[i+1];
+    }
+    users.pop();
+    delete userTasks[_addr];
+    isUser[_addr] = false;
+    isBanned[_addr] = true;
+
+    emit UserBanned(_addr);
+}
+
+//This function is used by the admins to unban a user
+function unBanUser(address _addr)public onlyRole(ADMIN_ROLE) validAddress(_addr){
+    isBanned[_addr] = false;
+      emit UserUnbanned(_addr);
+}
+
+// This function is used by the admins to check the ban status of a user
+function checkUserBanStatus(address _addr)public onlyRole(ADMIN_ROLE) validAddress(_addr) view returns(bool) {
+    return isBanned[_addr];
+}
+
+// This function is used by the admins to check the user status of an address
+function checkIsUserStatus(address _addr)public onlyRole(ADMIN_ROLE) validAddress(_addr) view returns(bool) {
+    return isUser[_addr];
+}
+
+// This function helps to get the information of a specific task a user created
+function getTaskInfo(uint _index)public view validIndex(_index) returns(uint, string memory, bool){
+   Task memory task = userTasks[msg.sender][_index];
+   return (task.id, task.description, task.isCompleted);
+}
+
+// This function helps users to know all their current tasks
+function getAllMyTasks()public view returns(Task[] memory){
+return userTasks[msg.sender];
+}
+
+// Allows Only admins to get all tasks by a user
+function getUserAllTasks(address _address)public view onlyRole(ADMIN_ROLE) validAddress(_address) returns(Task[] memory){
+    return userTasks[_address];
+}
+
+//Function to get addresses of all users by only admins
+function getAllUsersAddress()public view onlyRole(ADMIN_ROLE) returns(address[] memory){
+return users;
+}
+
+function getAllUsersAddressCount()public view onlyRole(ADMIN_ROLE)returns(uint){
+    return users.length;
+}
+
+// This function returns the total amount of tasks a user has created
+function getMyTasksCount()public view returns(uint){
+    return userTasks[msg.sender].length;
+}
+
+function getTotalAvailableTasks()public view onlyRole(DEFAULT_ADMIN_ROLE) returns(uint){
+     uint totalTasks = 0;
+for(uint i=0; i<users.length; i++){
+ totalTasks += userTasks[users[i]].length;
+}
+return totalTasks;
+}
+}
---
+++ test/ToDoList.t.sol
@@ -0,0 +1,302 @@
+// solhint-disable func-visibility, state-mutability
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "forge-std/Test.sol";
+import "../src/ToDoList.sol";
+
+contract ToDoListTest is Test{
+    ToDoList public todo;
+    address public owner;
+    address public zeroAddress;
+    address public user1;
+    address public user2;
+    address public user3;
+
+    function setUp() public{
+        owner = address(this);
+        zeroAddress = address(0);
+        user1 = makeAddr('user1');
+        user2 = makeAddr('user2');
+        user3 = makeAddr('user3');
+        todo = new ToDoList();
+    }
+
+// solhint-disable-next-line state-mutability
+    function testDeployerIsOwnerAndHaveRoles()public {
+        // checks if owner is deployer
+        assertEq(todo.owner(), owner);
+
+        // checks that deployer has Default admin role
+        bytes32 defaultAdmin = todo.DEFAULT_ADMIN_ROLE();
+        assertTrue(todo.hasRole(defaultAdmin, owner));
+
+        //  checks that deployer has admin role
+        bytes32 adminRole = todo.ADMIN_ROLE();
+        assertTrue(todo.hasRole(adminRole, owner));
+    }
+    function testAddTask()public{
+        string memory description = "learning foundry";
+        string memory desc1 = "";
+        string memory desc2 = "Play Chess";
+        todo.addTask(description);
+
+        (uint id, string memory desc, bool isCompleted) = todo.getTaskInfo(0);
+
+        assertEq(id, 1);
+        assertEq(desc, description);
+        assertEq(isCompleted, false);
+        assertTrue(todo.checkIsUserStatus(owner));
+
+    vm.expectRevert(ToDoList.InvalidDescription.selector);
+    todo.addTask(desc1);
+
+     vm.prank(user1);
+    todo.addTask(desc2);
+    todo.banUser(user1);
+    vm.expectRevert(ToDoList.RestrictedUser.selector);
+    vm.prank(user1);
+    todo.addTask(desc2);
+    }
+
+    function testEditTask()public{
+        string memory desc1 = "Do Chores";
+        string memory desc2 = "Play Games";
+        string memory desc3 = "Play Music";
+        string memory desc4 = "";
+
+        todo.addTask(desc1);
+        todo.addTask(desc2);
+        todo.editTask(desc3, 1);
+        (uint id, string memory description, bool isCompleted) = todo.getTaskInfo(1);
+        assertEq(id, 2);
+        assertEq(description, desc3);
+        assertEq(isCompleted, false);
+
+        vm.expectRevert(ToDoList.InvalidIndex.selector);
+        todo.editTask(desc2, 2);
+        vm.expectRevert(ToDoList.InvalidDescription.selector);
+        todo.editTask(desc4, 1);
+        todo.markTaskCompleted(1);
+        vm.expectRevert(ToDoList.TaskIsCompleted.selector);
+        todo.editTask(desc3, 1);
+    }
+
+    function testMarkTaskCompleted()public{
+     string memory desc1 = "Do Chores";
+
+        todo.addTask(desc1);
+        todo.markTaskCompleted(0);
+        (,,bool isCompleted) = todo.getTaskInfo(0);
+        assertEq(isCompleted, true);
+        vm.expectRevert(ToDoList.TaskIsCompleted.selector);
+        todo.markTaskCompleted(0);
+        vm.expectRevert(ToDoList.InvalidIndex.selector);
+        todo.markTaskCompleted(1);
+}
+
+    function testDeleteTask()public{
+     string memory desc1 = "Do Chores";
+     string memory desc2 = "Play Games";
+     string memory desc3 = "Work";
+        todo.addTask(desc1);
+        todo.addTask(desc2);
+        todo.addTask(desc3);
+        assertEq(todo.getMyTasksCount(), 3);
+        todo.delTask(0);
+        (,string memory description,) = todo.getTaskInfo(0);
+        (,string memory description2,) = todo.getTaskInfo(1);
+        assertEq(todo.getMyTasksCount(), 2);
+        assertEq(description, desc2);
+        assertEq(description2, desc3);
+
+        vm.expectRevert(ToDoList.InvalidIndex.selector);
+        todo.delTask(3);
+}
+
+function testAssignRole()public{
+    bytes32 role = todo.ADMIN_ROLE();
+
+    todo.assignRole(role, user1);
+    assertTrue(todo.hasRole(role, user1));
+
+    vm.expectRevert(ToDoList.InvalidAddress.selector);
+    todo.assignRole(role, zeroAddress);
+}
+
+function testBanUser()public{
+    string memory desc1 = "Do Chores";
+     string memory desc2 = "Play Games";
+    vm.prank(user1);
+    todo.addTask(desc1);
+    todo.banUser(user1);
+    vm.prank(user1);
+    vm.expectRevert(ToDoList.RestrictedUser.selector);
+    todo.addTask(desc2);
+
+    vm.expectRevert(ToDoList.NotYetUser.selector);
+    todo.banUser(user2);
+
+    vm.prank(user2);
+    todo.addTask(desc2);
+    vm.expectRevert();
+    vm.prank(user1);
+    todo.banUser(user2);
+}
+
+function testUnbanUser()public{
+     string memory desc1 = "Do Chores";
+     string memory desc2 = "Play Games";
+     string memory desc3 = "Read";
+    vm.startPrank(user1);
+    todo.addTask(desc1);
+    assertEq(todo.getMyTasksCount(), 1);
+    vm.stopPrank();
+    todo.banUser(user1);
+    vm.prank(user1);
+    vm.expectRevert(ToDoList.RestrictedUser.selector);
+    todo.addTask(desc2);
+    todo.unBanUser(user1);
+    vm.startPrank(user1);
+    todo.addTask(desc2);
+    todo.addTask(desc3);
+    assertEq(todo.getMyTasksCount(), 2);
+    vm.stopPrank();
+
+    vm.prank(user2);
+    todo.addTask(desc1);
+    todo.banUser(user2);
+    vm.expectRevert();
+    vm.prank(user1);
+    todo.unBanUser(user2);
+
+    vm.expectRevert(ToDoList.InvalidAddress.selector);
+    todo.unBanUser(zeroAddress);
+}
+
+function testCheckUserBanStatus()public{
+    string memory desc1 = "Do Chores";
+    vm.prank(user1);
+    todo.addTask(desc1);
+    assertFalse(todo.checkUserBanStatus(user1));
+    todo.banUser(user1);
+    assertTrue(todo.checkUserBanStatus(user1));
+}
+
+function testCheckIsUserStatus()public{
+    string memory desc1 = "Do Chores";
+   assertFalse(todo.checkIsUserStatus(user1));
+   vm.prank(user1);
+   todo.addTask(desc1);
+   assertTrue(todo.checkIsUserStatus(user1)); 
+}
+
+function testGetTaskInfo()public{
+    string memory desc1 = "Do Chores";
+    vm.startPrank(user1);
+    todo.addTask(desc1);
+    (uint id, string memory desc, bool isCompleted) = todo.getTaskInfo(0);
+    assertEq(id, 1);
+    assertEq(desc, desc1);
+    assertEq(isCompleted, false);
+    todo.markTaskCompleted(0);
+    (,, bool isComplete) = todo.getTaskInfo(0);
+    assertEq(isComplete, true);
+
+    vm.expectRevert(ToDoList.InvalidIndex.selector);
+    todo.getTaskInfo(1);
+}
+
+function testGetAllMyTasks()public{
+    string memory desc1 = "Do Chores";
+     string memory desc2 = "Play Games";
+     vm.startPrank(user1);
+    todo.addTask(desc1);
+    todo.addTask(desc2);
+  ToDoList.Task[] memory tasks = todo.getAllMyTasks();
+assertEq(tasks.length, 2);
+assertEq(tasks[0].description, desc1);
+assertEq(tasks[1].description, desc2);
+}
+
+function testGetUserAllTasks()public{
+    string memory desc1 = "Do Chores";
+     string memory desc2 = "Play Games";
+     vm.startPrank(user1);
+    todo.addTask(desc1);
+    todo.addTask(desc2);
+    vm.stopPrank();
+  ToDoList.Task[] memory tasks = todo.getUserAllTasks(user1);
+assertEq(tasks.length, 2);
+assertEq(tasks[0].description, desc1);
+assertEq(tasks[1].description, desc2);
+
+vm.expectRevert(ToDoList.InvalidAddress.selector);
+todo.getUserAllTasks(zeroAddress);
+}
+
+function testGetAllUsersAddress()public{
+ string memory desc1 = "Do Chores";
+     string memory desc2 = "Play Games";
+     string memory desc3 = "Read";
+    vm.prank(user1);
+    todo.addTask(desc1);
+    vm.prank(user2);
+    todo.addTask(desc2);
+    vm.prank(user3);
+    todo.addTask(desc3);
+    assertEq((todo.getAllUsersAddress()).length,3);
+    todo.banUser(user2);
+    assertEq((todo.getAllUsersAddress()).length, 2);
+}
+
+function testGetAllUsersAddressCount()public{
+    string memory desc1 = "Do Chores";
+     string memory desc2 = "Play Games";
+     string memory desc3 = "Read";
+     vm.prank(user1);
+    todo.addTask(desc1);
+    vm.prank(user2);
+    todo.addTask(desc2);
+    vm.prank(user3);
+    todo.addTask(desc3);
+    assertEq(todo.getAllUsersAddressCount(), 3);
+    todo.banUser(user2);
+    assertEq(todo.getAllUsersAddressCount(), 2);
+}
+
+function testGetMyTasksCount()public{
+      string memory desc1 = "Do Chores";
+     string memory desc2 = "Play Games";
+     string memory desc3 = "Watch Ithachi";
+     vm.startPrank(user1);
+    todo.addTask(desc1);
+    todo.addTask(desc2);
+    todo.addTask(desc3);
+    assertEq(todo.getMyTasksCount(), 3);
+    todo.delTask(1);
+    assertEq(todo.getMyTasksCount(), 2);
+}
+
+function testGetTotalAvailableTasks()public{
+    string memory desc1 = "Do Chores";
+     string memory desc2 = "Play Games";
+     string memory desc3 = "Watch Ithachi";
+     string memory desc4 = "Play football";
+     string memory desc5 = "Take Coffee";
+     vm.startPrank(user1);
+    todo.addTask(desc1);
+    todo.addTask(desc2);
+    todo.addTask(desc3);
+    vm.stopPrank();
+    vm.prank(user2);
+    todo.addTask(desc4);
+    vm.prank(user3);
+    todo.addTask(desc5);
+    assertEq(todo.getTotalAvailableTasks(), 5);
+    vm.prank(user1);
+    todo.delTask(0);
+todo.banUser(user2);
+assertEq(todo.getTotalAvailableTasks(), 3);
+}
+}
